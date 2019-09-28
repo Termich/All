ae0dbda52f4efb268f8d849eff88446b6fb0710f
Termich <p_artem@bk.ru>
2019-09-28 18:16:58 +0300

    Делаю 2 е задание

--- Lesson 5/Modul_3.py
+++ Lesson 5/Modul_3.py
@@ -1,14 +1,7 @@
 from random import randint,choice
 
-
-# Создаем функцию
 def live(*args):
    return choice(args)
 
 num = list(map(int, input('Введите 4 числа через запятую, в одной строке.').split(',')))
-
-if num == int:
-    print(num)
-else:
-    print('None')
-
+print(live(*num))
