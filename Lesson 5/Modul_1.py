57e0a93801395361f6a7faa61a21e1332077e963
Termich <p_artem@bk.ru>
2019-09-28 19:46:54 +0300

    А я оказываеться не создал функцию для первого задания

--- Lesson 5/Modul_1.py
+++ Lesson 5/Modul_1.py
@@ -1,7 +1,21 @@
 import sys, os
 
-name = 'dir'
 
-for i in range(1,9) :
-    path = os.path.join(os.getcwd(), '{}_{}'.format(name,i))
-    os.mkdir(path)
+
+#for i in range(1,9) :
+#    path = os.path.join(os.getcwd(), '{}_{}'.format(name,i))
+#    os.mkdir(path)
+
+
+
+def create (name,range):
+    for i in range():
+        path = os.path.join(os.getcwd(), '{}_{}'.format(name, i))
+        os.mkdir(path)
+
+create(dir(1,9))
+
+
+
+
+print('Выполнить создания папок?')
