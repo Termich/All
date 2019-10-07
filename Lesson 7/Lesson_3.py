18e436f5edf364819bf3a5831ab4a4f6c634ef74
Termich <p_artem@bk.ru>
2019-10-07 16:26:02 +0300

    Вот так это работает

--- Lesson 7/Lesson_3.py
+++ Lesson 7/Lesson_3.py
@@ -1,9 +1,11 @@
 key = [1, 5, 8, 9, 20, 27, 45, 68, 41, -5, -10, -16, -55, 12]
 bey = key.copy()
 
-result2 = [number**2 for number in key and key > 0]
+result1 = [number for number in key if number > 0]
+result2 = [number**2 for number in result1]
 print(result2)
-
+result3 = [number for number in key if number < 0]
+print(result3)
 #def numbers (key):
 #    newnumber = int[number for number in key if key**2]
 
