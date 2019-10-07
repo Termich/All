fc0034944e429cb6dd08fe77618ac927e2ed764f
Termich <p_artem@bk.ru>
2019-10-07 16:04:07 +0300

    Не выходит каменный цветок

--- Lesson 7/Lesson_3.py
+++ Lesson 7/Lesson_3.py
@@ -1,7 +1,10 @@
 key = [1, 5, 8, 9, 20, 27, 45, 68, 41, -5, -10, -16, -55, 12]
+bey = key.copy()
 
+result2 = [number for number in key if number**4 > 0]
+print(result2)
 
-def numbers (key):
-    newnumber = int[number for number in key if key**2 ]
+#def numbers (key):
+#    newnumber = int[number for number in key if key**2]
 
-print(numbers(key))
+#print(numbers(key))
