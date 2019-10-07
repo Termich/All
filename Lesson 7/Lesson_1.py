bf876c4efd94f89b2d1f30de4ffcc341ad9039a2
Termich <p_artem@bk.ru>
2019-10-07 12:14:53 +0300

    Сделал

--- Lesson 7/Lesson_1.py
+++ Lesson 7/Lesson_1.py
@@ -3,6 +3,8 @@ a = ['Яблоко','Груша','Бургер']
 
 b = ['Ананас','Нектарин','Шаурма']
 
-for result in a + b:
-    print(result)
-    print(type)
+#for result in a + b:
+#    print(result)
+
+banana = [result for result in a + b]
+print(banana)
