06929870f14df8116f33b3dd617548445749fc94
Termich <p_artem@bk.ru>
2019-10-07 14:06:20 +0300

    Первое задание сделал не верно

--- Lesson 7/Lesson_1.py
+++ Lesson 7/Lesson_1.py
@@ -1,10 +1,10 @@
 
-a = ['Яблоко','Груша','Бургер']
+a = ['Яблоко','Груша','Бургер','Пиво']
 
-b = ['Ананас','Нектарин','Шаурма']
+b = ['Ананас','Пиво','Нектарин','Шаурма']
 
 #for result in a + b:
 #    print(result)
 
-banana = [result for result in a + b]
-print(banana)
+#banana = [result for result in a + b]
+#print(banana)
