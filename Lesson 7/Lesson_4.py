18bbba6fcb93552ff271e43aa3105c1ec19a5c6a
Termich <p_artem@bk.ru>
2019-10-08 13:22:18 +0300

    Пришел Артем и опять помог мне

--- Lesson 7/Lesson_4.py
+++ Lesson 7/Lesson_4.py
@@ -1,16 +1,16 @@
 number = int(input('Введите число: '))
 
-#def banana(number):
-  #  if number == 13:
-  #      raise ValueError
- #   else:
- #       number ** 2
+def banana(number):
+    if number == 13:
+        raise ValueError
+    else:
+      return number ** 2
 
 
-#print(banana())
 
-def banan(number):
-    aba = [number if number == 13 else number ** 2]
-    raise ValueError
-    return aba
-print(banan(number))
+# print(banana())
+#
+# def banan(number):
+#     num = -1    if number == 13 else number ** 2
+#     if num ==-1 : raise ValueError
+#     return number
--- Test/Test.py
+++ Test/Test.py
@@ -1,16 +1,4 @@
-#тут введя 4 числа мы получим случайное из них
-
-
-from random import choice
-
-
-
-#тут введя 4 числа мы получим случайное из них
-
-
-def special(some_list):
-    if some_list: return (some_list)
-
-
-key = [1,2,3,4]
-print(choice(special(key)))
+try:
+    print(banan(number))
+except ValueError:
+    print(' пнх дружище')
