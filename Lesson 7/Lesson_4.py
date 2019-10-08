518ed6055a01595acadff5a92274e982e5b56828
Termich <p_artem@bk.ru>
2019-10-08 12:20:30 +0300

    Делаю

--- Lesson 7/Lesson_4.py
+++ Lesson 7/Lesson_4.py
@@ -1,5 +1,10 @@
 number = int(input('Введите число: '))
 
+def banana(number):
+    if number == 13 raise ValueError
+    result = [math.sqrt(number) if number > 0 else number for number in key]
+
+
 
 
 try:
