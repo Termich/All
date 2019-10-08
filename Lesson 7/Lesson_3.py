046e037da30d0fb7fc6226ea3d6884a93f09c6b0
Termich <p_artem@bk.ru>
2019-10-08 10:32:24 +0300

    К успеху шел шел и пришел

--- Lesson 7/Lesson_3.py
+++ Lesson 7/Lesson_3.py
@@ -2,15 +2,16 @@ import math
 
 
 key = [1, -3, 4]
-bey = key.copy()
 
-result1 = [number for number in key if number > 0]
-result2 = [math.sqrt(number) for number in result1]
-print(result2)
+result1 = [math.sqrt(number) for number in key if number > 0]
+
 result3 = [number for number in key if number < 0]
+
+
+
+print(result1)
 print(result3)
 #def numbers (key):
 #    newnumber = int[number for number in key if key**2]
 
 #print(numbers(key))
-
