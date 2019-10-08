9859cd7387c2dcd17e101263dc36e634f478efd4
Termich <p_artem@bk.ru>
2019-10-08 12:39:57 +0300

    Через тернарный не получилось

--- Lesson 7/Lesson_4.py
+++ Lesson 7/Lesson_4.py
@@ -3,11 +3,11 @@ number = int(input('Введите число: '))
 #def banana(number):
 #    if number == 13:
 #        raise ValueError
-#    else:
-#        number * 2
 
-#print(banana(number))
+
+#print(banana())
 
 def banan(number):
-    result = [bumber * 2 for bumber in number if number == 13 raise ValueError]
-    return result
+    aba = [raise ValueError if number == 13 else number * 2]
+
+print(banan())
