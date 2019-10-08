920a9eb226261b5ccd4fb4d20548ded2d10ceddd
Termich <p_artem@bk.ru>
2019-10-08 12:48:15 +0300

    Допустим так работает, но надо возвести в степень

--- Lesson 7/Lesson_4.py
+++ Lesson 7/Lesson_4.py
@@ -1,13 +1,15 @@
 number = int(input('Введите число: '))
 
 #def banana(number):
-#    if number == 13:
-#        raise ValueError
+  #  if number == 13:
+  #      raise ValueError
+ #   else:
+ #       number ** 2
 
 
 #print(banana())
 
 def banan(number):
-    aba = [raise ValueError if number == 13 else number * 2]
-
-print(banan())
+    aba = [number if number == 13 else number ** 2]
+    return aba
+print(banan(number))
