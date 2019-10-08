d98723929aec8727dfbd0e25f4e063e3600955e4
Termich <p_artem@bk.ru>
2019-10-08 12:32:31 +0300

    Пытасюсь что то сделать

--- Lesson 7/Lesson_4.py
+++ Lesson 7/Lesson_4.py
@@ -1,9 +1,13 @@
 number = int(input('Введите число: '))
 
-def banana(number):
-    if number == 13:
-        raise ValueError
-    else:
-        number * 2
+#def banana(number):
+#    if number == 13:
+#        raise ValueError
+#    else:
+#        number * 2
 
-print(banana(number))
+#print(banana(number))
+
+def banan(number):
+    result = [bumber * 2 for bumber in number if number == 13 raise ValueError]
+    return result
