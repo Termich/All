e1cdba3378aaab37abfd38f6ba62a431c772875b
Termich <p_artem@bk.ru>
2019-10-09 14:30:56 +0300

    path не видит os - будем разбираться

--- Lesson 8/step1.py
+++ Lesson 8/step1.py
@@ -1,10 +1,9 @@
 import os
-
-
-def create_file(name, text = None):
+def create_file(name, text=None):
     with open(name, 'w', encoding='utf-8') as f:
         if text:
             f.write(text)
+
 
 def create_folder(name):
     try:
@@ -13,10 +12,15 @@ def create_folder(name):
         print('Такая папка уже есть')
 
 
+def get_list(folder_only=False):
+    result = os.listdir()
+    if folder_only:
+        result = [f for f in result if os.patch.isdir(f)]
+    print(result)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     create_file('test.dat')
     create_file('test.dat', 'test2')
     create_folder('Folder test')
-
-
+    get_list(True)
