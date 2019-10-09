8acc496fa409ac0a0c311696b877c25f9dc36f41
Termich <p_artem@bk.ru>
2019-10-09 15:22:08 +0300

    А теперь не работает shutil

--- Lesson 8/step1.py
+++ Lesson 8/step1.py
@@ -1,4 +1,6 @@
 import os
+
+
 def create_file(name, text=None):
     with open(name, 'w', encoding='utf-8') as f:
         if text:
@@ -15,12 +17,38 @@ def create_folder(name):
 def get_list(folder_only=False):
     result = os.listdir()
     if folder_only:
-        result = [f for f in result if os.patch.isdir(f)]
+        result = [f for f in result if os.path.isdir(f)]
     print(result)
+
+
+def delete_file(name):
+    if os.path.isdir(name):
+        os.rmdir(name)
+    else:
+        os.remove(name)
+
+
+def copy_file(name, new_name):
+    if os.path.isdir(name):
+        try:
+            shutil.copyfree(name, new_name)
+        except FileExistsError:
+            print('Таккая папка уже есть')
+    else:
+        shutil.copy(name, new_name)
+
+
 
 
 if __name__ == '__main__':
     create_file('test.dat')
+    create_file('test12.dat')
     create_file('test.dat', 'test2')
-    create_folder('Folder test')
+    create_folder('New_f1')
+    create_folder('New_f2')
     get_list(True)
+    get_list()
+    delete_file('New_f1')
+    delete_file('test.dat')
+    copy_file('test12.dat','test14.dat')
+    copy_file('New_f2','New_f3')
