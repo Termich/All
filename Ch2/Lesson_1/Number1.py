505be291c4fd8aba9ea4988771273f2e8a3cbfe0
Termich <p_artem@bk.ru>
2019-10-17 18:59:46 +0300

    Потихоньку идем

--- Ch2/Lesson_1/Number1.py
+++ Ch2/Lesson_1/Number1.py
@@ -5,11 +5,17 @@ with open("text.txt", "r") as file:
     # print(content)
 
 patern1 = r'(?<=\w[.!?])'
-patern2 = re.findall("\w+\.{3}" , content)
 
-print(patern2)
+words = re.compile('\w+')
+print(words.findall(content))
 
-result = re.split(patern1, content)
+
+
+#patern2 = re.findall("(\w)" , content)
+
+#print(patern2)
+
+#result = re.split(patern1, content)
 
 #print(result)
 
