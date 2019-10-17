db87450f7acd1f89104fa0f8b251235f3ccb5974
Termich <p_artem@bk.ru>
2019-10-17 18:38:56 +0300

    Сделал 1 и 2е Теперь пытаюсь найти слова

--- Ch2/Lesson_1/Number1.py
+++ Ch2/Lesson_1/Number1.py
@@ -1,3 +1,15 @@
-with open("text.txt" , "r") as file:
+import re
+
+with open("text.txt", "r") as file:
     content = file.read()
-    print(content)
+    # print(content)
+
+patern1 = r'(?<=\w[.!?])'
+patern2 = re.findall("\w+\.{3}" , content)
+
+print(patern2)
+
+result = re.split(patern1, content)
+
+#print(result)
+
