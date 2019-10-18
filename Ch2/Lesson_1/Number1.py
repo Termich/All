40a697515f99309a64c12a321ca1821d2f30f2aa
Termich <p_artem@bk.ru>
2019-10-18 14:07:38 +0300

    Сделал, подчищаю

--- Ch2/Lesson_1/Number1.py
+++ Ch2/Lesson_1/Number1.py
@@ -1,29 +1,40 @@
 import re
+from collections import Counter
 
+# Получил текст из файла
 with open("text.txt", "r", encoding='utf-8') as file:
     content = file.read()
- #   print(content)
-print(content)
 
+# Разбил полученный текст на предложения.
 patern1 = re.compile('[\.\?!]\s+')
 result = re.split(patern1, content)
-#print(result)
 
-
+# Нашел слова состоящие из 4 букв
 words = re.compile('\w{4,}')
-# print(words.findall(content))
+
+cnt = Counter(x for x in re.findall('[\w]+\.[\w]+', content))
+
+# Отобрал все ссылки
+links = re.compile('[\w\.]+\.[\w/]+')
+
+#Возврат ссылок которые встречаються чаще всего
 
 
+# Замените все ссылки на текст:
+Registration = re.sub(links, "'Ссылка отобразится после регистрации'", content)
 
 
-
-#patern2 = re.findall("(\w)" , content)
-
-#print(patern2)
-
-links = re.compile('[\w\.:/]+\.[\w/\.]+')
+print("Получил текст из файла")
+print(content)
+print("Разбил текс на предложения")
+print(result)
+print("Нашел слова состоящие из 4-х букв")
+print(words.findall(content))
+print('10 самых часто используемых слов')
+print(cnt.most_common(10))
+print("Нашел все ссылки")
 print(links.findall(content))
-#print(links)
-
-
-
+print("Самая часто употребляемая ссылка")
+print(cnt.most_common(1))
+print("Ссылка отобразиться после регистрации")
+print(Registration)
