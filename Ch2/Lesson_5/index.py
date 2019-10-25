a3bcaea88e58386a7235ef7335f22a049cd36356
Termich <p_artem@bk.ru>
2019-10-25 15:30:14 +0300

    Ссылку получил, но не могу найти текст

--- Ch2/Lesson_5/index.py
+++ Ch2/Lesson_5/index.py
@@ -1,7 +1,10 @@
 import re
+import requests
 from bs4 import BeautifulSoup as BS
 from wiki_requests import get_topic_page
 from wiki_requests import get_link
+
+
 
 def get_topic_words(topic):
     html_content = get_topic_page(topic)
@@ -24,20 +27,33 @@ def visualize_common_words(topic):
     words = get_common_words(topic)
     for w in words[3:6]:
         print(w[0])
+    return words
 
 def main():
     topic = input("Topic: ")
     visualize_common_words(topic)
 
-Fink = print(visualize_common_words(input("ВВедите значение")))
+
+def get_http(topic):
+    html_content = get_topic_page(topic)
+    text = requests.get(html_content).text
+    data = BS(text,"html.parser")
+    result = data.find_all("div id",class_="mw-parser-output",text="/wiki/")
+    return result
+
+
+
+#Fink = print(visualize_common_words(input("ВВедите значение")))
+print(get_http(input("ВВедите значение")))
+
 
 # li = re.findall('<p><a href="/wiki/([\d\s]+)"></a></p>',Fink)
 # print(li)
-
-with open (get_link,encoding='utf-8') as f:
-    s = f.read()
-soup = BS(s,"html.parser")
-print(soup)
+#
+# with open (get_link,encoding='utf-8') as f:
+#     s = f.read()
+# soup = BS(s,"html.parser")
+# print(soup)
 
 # t= soup.find_all("div id",class_="mw-parser-output",text="/wiki/")
 # print(t)
