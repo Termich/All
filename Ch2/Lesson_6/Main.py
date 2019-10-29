3c6552a3b212a061820ea75c12d5ef12ce0a84a6
Termich <p_artem@bk.ru>
2019-10-29 16:04:23 +0300

    Закончили делать, была проблема с генераторами

--- Ch2/Lesson_6/Main.py
+++ Ch2/Lesson_6/Main.py
@@ -11,33 +11,49 @@ class Sentence:
         self.content = content
 
     def show(self):
-        for x in words:
-            words.append(Word(x[0], x[1]))
-
-
-
+        sentence = ' '.join([words[x].text for x in self.content])
+        return sentence
+    def show_parts(self):
+        parts = [words[x].part for x in self.content]
+        parts = list(set(parts))
+        return parts
 
 
 li = [["собака", "сущ"],
-          ["ела", "глагол"],
-          ["колбасу", "сущ"],
-          ["вечером", "наречие"]]
+      ["ела", "глагол"],
+      ["колбасу", "сущ"],
+      ["вечером", "наречие"]]
 
-papana = Sentence([0,1,2,3])
+papana = Sentence([0, 1, 2, 3])
 banana = Word("собака", "сущ")
 apana = Word("ела", "глагол")
 
-words = []
-for word in li:
-    words.append(Word(word[0], word[1]))
+words = [Word(word[0], word[1]) for word in li]
+
+print(papana.show())
+print(papana.show_parts())
+
+
+
+
+# for word in li:
+#     words.append(Word(word[0], word[1]))
+
+
+# content = [3,0,1,2]
+# sentence = ' '.join([words[x].text for x in content])
+# # sentence =[]
+# # for x in content:
+# #     sentence.append(words[x].text)
+# # sentence = ' '.join(sentence)
+# print(sentence)
+
 
 # for i in range(len(words1)):
 #     words1[i] = Word(words1[i][0], words1[i][1])
 
 
-
-
-print(papana.content)
-print(words[0].text)
-print(banana.text, apana.text)
-print(banana.part, apana.part)
+# print(papana.show())
+# print(words[0].text)
+# print(banana.text, apana.text)
+# print(banana.part, apana.part)
