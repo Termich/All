6d54a8b18bc1c420e33f6eedd027d928747d90f3
Termich <p_artem@bk.ru>
2019-10-29 12:50:04 +0300

    Потихоньку делаю задание

--- Ch2/Lesson_6/Main.py
+++ Ch2/Lesson_6/Main.py
@@ -1,17 +1,23 @@
 class Word:
-    def text(self):
-        text = self.text
-        print(text)
 
-    def Sentence(self):
-        Sentence = self.Sentence
-        print(Sentence)
-
-    def __init__(self, word, part, content):
-        self.word = word
+    def __init__(self, text, part):
+        self.text = text
         self.part = part
+
+
+class Sentence:
+
+    def __init__(self, text, part):
         self.content = []
         self.words = [["собака", "сущ"],
                       ["ела", "глагол"],
                       ["колбасу", "сущ"],
                       ["вечером", "наречие"]]
+
+
+
+banana = Word("собака","сущ")
+apana = Word("ела","глагол")
+
+print(banana.text,apana.text)
+print(banana.part,apana.part)
