dff14deca33ff0f0b6a69a69c852d52daa87c7e1
Termich <p_artem@bk.ru>
2019-10-29 14:32:30 +0300

    Пишел Артем и помог, начала делать пункт 7

--- Ch2/Lesson_6/Main.py
+++ Ch2/Lesson_6/Main.py
@@ -7,23 +7,37 @@ class Word:
 
 class Sentence:
 
-    # words = [["собака", "сущ"],
-    #          ["ела", "глагол"],
-    #          ["колбасу", "сущ"],
-    #          ["вечером", "наречие"]]
+    def __init__(self, content):
+        self.content = content
 
-    def __init__(self, words):
-        self.content = words[["собака", "сущ"],
-             ["ела", "глагол"],
-             ["колбасу", "сущ"],
-             ["вечером", "наречие"]]
+    def show(self):
+        for x in words:
+            words.append(Word(x[0], x[1]))
 
 
+
+
+
+li = [["собака", "сущ"],
+          ["ела", "глагол"],
+          ["колбасу", "сущ"],
+          ["вечером", "наречие"]]
+
+papana = Sentence([0,1,2,3])
 banana = Word("собака", "сущ")
 apana = Word("ела", "глагол")
-papana = Sentence(0,2)
+
+words = []
+for word in li:
+    words.append(Word(word[0], word[1]))
+
+# for i in range(len(words1)):
+#     words1[i] = Word(words1[i][0], words1[i][1])
+
+
 
 
 print(papana.content)
+print(words[0].text)
 print(banana.text, apana.text)
 print(banana.part, apana.part)
