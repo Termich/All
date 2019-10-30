d366b08784392b0e6c1f0f5d6a2b1734c8f716c4
Termich <p_artem@bk.ru>
2019-10-30 17:26:11 +0300

    Делаю

--- Ch2/Lesson_7/Main.py
+++ Ch2/Lesson_7/Main.py
@@ -1,9 +1,7 @@
 
 class Word:
-    def __init__(self, text, part):
+    def __init__(self, text):
         self.text = text
-        self.part = part
-
 
 class Sentence:
 
@@ -21,24 +19,25 @@ class Sentence:
         parts = list(set(parts))
         return parts
 
-li = [["собака", "сущ"],
+if __name__ == '__main__':
+    li = [["собака", "сущ"],
       ["ела", "глагол"],
       ["колбасу", "сущ"],
       ["вечером", "наречие"]]
 
-papana = Sentence([0, 1, 2, 3])
+    papana = Sentence([0, 1, 2, 3])
 
 
-banana = Word("собака", "сущ")
-apana = Word("ела", "глагол")
+    banana = Word("собака", "сущ")
+    apana = Word("ела", "глагол")
 # print(banana.text, apana.text)
 # print(banana.part, apana.part)
 
 
-words = [Word(word[0], word[1]) for word in li]
+    words = [Word(word[0], word[1]) for word in li]
 
-print(papana.show())
-print(papana.show_parts())
+    print(papana.show())
+    print(papana.show_parts())
 
 # for word in li:
 #     words.append(Word(word[0], word[1]))
--- Ch2/Lesson_7/Main_2.py
+++ Ch2/Lesson_7/Main_2.py
@@ -1,15 +1,32 @@
-from Main import Word
-from Main import Sentence
+
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+class Sentence:
+
+    def __init__(self, content):
+        self.content = content
+
+
+    def show(self):
+        sentence = ' '.join([words[x].text for x in self.content])
+        return sentence
+
+
+    def show_parts(self):
+        parts = [words[x].part for x in self.content]
+        parts = list(set(parts))
+        return parts
 
 class Noun(Word):
-    def __init__(self):
-        self.__grammar = "сущ"
+    __grammar = "сущ"
+    # def __init__(self):
+
 
 class Verb(Word):
-    def __init__(self):
-        self.__grammar = "гл"
-
-
+    __grammar = "гл"
+    # def __init__(self):
 
 
 words = []
